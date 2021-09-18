75d9838866b11a2c30b08aba8f6559391a4c5239
Jan Rejnowski <jan.rejnowski@gmail.com>
2021-09-18 19:19:18 +0200

    Added Language Model;

---
+++ src/main/java/com/pjatk/medicalcenter/model/Language.java
@@ -0,0 +1,35 @@
+package com.pjatk.medicalcenter.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public abstract class Language {
+
+    @Id
+    @SequenceGenerator(
+            name = "language_sequence",
+            sequenceName = "language_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "language_sequence"
+    )
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String code;
+
+}
