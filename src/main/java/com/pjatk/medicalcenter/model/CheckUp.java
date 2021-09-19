71a8479219a03e867dd8c36c05fea15a13c75862
Jan Rejnowski <jan.rejnowski@gmail.com>
2021-09-19 12:27:33 +0200

    Small refactor made; Ready to merge with dev;

--- src/main/java/com/pjatk/medicalcenter/model/CheckUp.java
+++ src/main/java/com/pjatk/medicalcenter/model/CheckUp.java
@@ -20,5 +20,5 @@ public class CheckUp {
 
     @Column(nullable = false)
     private String name;
-    
+
 }
--- src/main/java/com/pjatk/medicalcenter/service/CheckUpService.java
+++ src/main/java/com/pjatk/medicalcenter/service/CheckUpService.java
@@ -42,4 +42,5 @@ public class CheckUpService {
 
         checkUpRepository.delete(checkUp);
     }
+
 }
