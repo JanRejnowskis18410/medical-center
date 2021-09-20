d114b57f3a54f9cc448663f856ba8f2ce7ffeb9b
Jan Rejnowski <jan.rejnowski@gmail.com>
2021-09-20 22:23:04 +0200

    Added many-to-many relation between Appointment and Language;

--- src/main/java/com/pjatk/medicalcenter/model/Appointment.java
+++ src/main/java/com/pjatk/medicalcenter/model/Appointment.java
@@ -7,16 +7,25 @@ import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Data
 @NoArgsConstructor
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "appointment_type")
 public abstract class Appointment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToMany
+    @JoinTable(name = "appointment_language",
+            joinColumns = {@JoinColumn(name = "appointment_id")},
+            inverseJoinColumns = {@JoinColumn(name = "language_id")})
+    private Set<Language> languages = new HashSet<>();
 
     @Column(
             nullable = false
--- src/main/java/com/pjatk/medicalcenter/model/Language.java
+++ src/main/java/com/pjatk/medicalcenter/model/Language.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @AllArgsConstructor
@@ -18,10 +20,12 @@ public class Language {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToMany(mappedBy = "languages")
+    private Set<Appointment> appointments = new HashSet<>();
+
     @Column(nullable = false)
     private String name;
 
     @Column(nullable = false)
     private String code;
-
 }
