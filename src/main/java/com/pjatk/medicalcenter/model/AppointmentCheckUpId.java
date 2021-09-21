ebab38a5ee33f0ecf6b4906a5d159123a3c7bc38
Jan Rejnowski <jan.rejnowski@gmail.com>
2021-09-21 10:51:08 +0200

    Added lombok annotations in AppointmentCheckUpId instead of equals and hashCode methods.

--- src/main/java/com/pjatk/medicalcenter/model/AppointmentCheckUpId.java
+++ src/main/java/com/pjatk/medicalcenter/model/AppointmentCheckUpId.java
@@ -1,46 +1,20 @@
 package com.pjatk.medicalcenter.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.Embeddable;
 import java.io.Serializable;
-import java.util.Objects;
 
 @Embeddable
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
+@Data
 public class AppointmentCheckUpId implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private Long checkUpId;
     private Long appointmentId;
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result
-                + ((checkUpId == null) ? 0 : checkUpId.hashCode());
-        result = prime * result
-                + ((appointmentId == null) ? 0 : appointmentId.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        AppointmentCheckUpId other = (AppointmentCheckUpId) obj;
-        return Objects.equals(getCheckUpId(), other.getCheckUpId()) && Objects.equals(getAppointmentId(), other.getAppointmentId());
-    }
 }
