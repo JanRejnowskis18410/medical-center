33ab1205840e71aa1bdd017cc1c144d03977560c
Jan Rejnowski <jan.rejnowski@gmail.com>
2021-10-03 22:12:55 +0200

    Added numberOfPackages and dosing fields in PrescriptionMedication;

--- src/main/java/com/pjatk/medicalcenter/model/PrescriptionMedication.java
+++ src/main/java/com/pjatk/medicalcenter/model/PrescriptionMedication.java
@@ -1,9 +1,6 @@
 package com.pjatk.medicalcenter.model;
 
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.MapsId;
+import javax.persistence.*;
 
 @Entity
 public class PrescriptionMedication {
@@ -18,4 +15,10 @@ public class PrescriptionMedication {
     @ManyToOne
     @MapsId("medicationId")
     private Medication medication;
+
+    @Column(nullable = false)
+    private int numberOfPackages;
+
+    @Column(nullable = false)
+    private String dosing;
 }
