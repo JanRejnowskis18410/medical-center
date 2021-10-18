a92d546c07f467424d194579a256ae8db688a105
Jan Rejnowski <jan.rejnowski@gmail.com>
2021-10-18 22:32:34 +0200

    Deleted redundant classes: Language, LanguageController, LanguageRepository, LanguageService;

--- src/main/java/com/pjatk/medicalcenter/repository/LanguageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pjatk.medicalcenter.repository;
-
-import com.pjatk.medicalcenter.model.Language;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LanguageRepository extends JpaRepository<Language, Long> {
-}
