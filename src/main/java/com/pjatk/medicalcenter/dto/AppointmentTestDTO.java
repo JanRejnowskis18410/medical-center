68e8c82a864fa3d961f52f22e0bd48ff749ce259
Weronika18991 <s18991@pjwstk.edu.pl>
2021-11-22 07:57:24 +0100

    added appointmentDate to AppointmentTestDTO

--- src/main/java/com/pjatk/medicalcenter/dto/AppointmentTestDTO.java
+++ src/main/java/com/pjatk/medicalcenter/dto/AppointmentTestDTO.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -12,6 +14,7 @@ public class AppointmentTestDTO {
 
     private long appointmentId;
     private long checkUpId;
+    private LocalDateTime appointmentDate;
     private String diagnosticTestName;
     private String result;
     private String description;
@@ -20,6 +23,7 @@ public class AppointmentTestDTO {
     public AppointmentTestDTO(AppointmentCheckUp appointmentCheckUp) {
         this.appointmentId = appointmentCheckUp.getAppointment().getId();
         this.checkUpId = appointmentCheckUp.getCheckUp().getId();
+        this.appointmentDate = appointmentCheckUp.getAppointment().getDate();
         this.diagnosticTestName = appointmentCheckUp.getCheckUp().getName();
         this.result = appointmentCheckUp.getResult();
         this.description = appointmentCheckUp.getDescription();
