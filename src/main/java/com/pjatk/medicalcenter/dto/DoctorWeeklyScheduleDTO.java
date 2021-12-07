5beafbefe014a9dc5a76db4f81d7eb28f1d86ffa
Weronika18991 <s18991@pjwstk.edu.pl>
2021-12-07 14:05:07 +0100

    changed DoctorWeeklyScheduleDTO structure

--- src/main/java/com/pjatk/medicalcenter/dto/DoctorWeeklyScheduleDTO.java
+++ src/main/java/com/pjatk/medicalcenter/dto/DoctorWeeklyScheduleDTO.java
@@ -1,37 +1,32 @@
 package com.pjatk.medicalcenter.dto;
 
-import com.pjatk.medicalcenter.model.Schedule;
-import com.pjatk.medicalcenter.model.Schedule.DayOfWeek;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.HashMap;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class DoctorWeeklyScheduleDTO {
 
-    private Map<Schedule.DayOfWeek,List<ScheduleDTO>> weeklySchedule = new HashMap<>();
+    private List<Schedule> weeklySchedule = new ArrayList<>();
 
     public DoctorWeeklyScheduleDTO(List<ScheduleDTO> schedules){
-        weeklySchedule.put(DayOfWeek.MONDAY,schedules.stream()
-                .filter(s -> s.getDayOfWeek().equals(DayOfWeek.MONDAY)).collect(Collectors.toList()));
-        weeklySchedule.put(DayOfWeek.TUESDAY,schedules.stream()
-                .filter(s -> s.getDayOfWeek().equals(DayOfWeek.TUESDAY)).collect(Collectors.toList()));
-        weeklySchedule.put(DayOfWeek.WEDNESDAY,schedules.stream()
-                .filter(s -> s.getDayOfWeek().equals(DayOfWeek.WEDNESDAY)).collect(Collectors.toList()));
-        weeklySchedule.put(DayOfWeek.THURSDAY,schedules.stream()
-                .filter(s -> s.getDayOfWeek().equals(DayOfWeek.THURSDAY)).collect(Collectors.toList()));
-        weeklySchedule.put(DayOfWeek.FRIDAY,schedules.stream()
-                .filter(s -> s.getDayOfWeek().equals(DayOfWeek.FRIDAY)).collect(Collectors.toList()));
-        weeklySchedule.put(DayOfWeek.SATURDAY,schedules.stream()
-                .filter(s -> s.getDayOfWeek().equals(DayOfWeek.SATURDAY)).collect(Collectors.toList()));
-        weeklySchedule.put(DayOfWeek.SUNDAY,schedules.stream()
-                .filter(s -> s.getDayOfWeek().equals(DayOfWeek.SUNDAY)).collect(Collectors.toList()));
+        schedules.forEach(sch -> weeklySchedule.add(new Schedule(sch.getDateFrom(), sch.getDateTo())));
+    }
+
+    @Getter
+    public static class Schedule {
+        LocalDateTime startDate;
+        LocalDateTime endDate;
+
+        Schedule(LocalDateTime startDate, LocalDateTime endDate){
+            this.startDate = startDate;
+            this.endDate = endDate;
+        }
     }
 }
